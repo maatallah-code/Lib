56d1cc287efd41e59a06b9e72306faeb37c300bb
maatallah <maatallahabdelkader3@gmail.com>
2023-06-02 11:11:38 +0100

    Initial commit

---
+++ app/src/main/java/com/maatallah/lib/adapters/OnCountrySelected.kt
@@ -0,0 +1,12 @@
+package com.maatallah.lib.adapters
+
+import com.yasserakbbach.phonenumberpicker.models.Country
+
+/**
+ * To handle on country selected
+ * @author: Yasser AKBBACH
+ */
+interface OnCountrySelected {
+
+    fun setOnCountrySelected(country: Country)
+}
---
+++ app/src/main/java/com/maatallah/lib/models/Continent.kt
@@ -0,0 +1,15 @@
+package com.maatallah.lib.models
+
+/**
+ * List of all available continents,
+ * @author: Yasser AKBBACH
+ */
+enum class Continent {
+
+    AFRICA,
+    ASIA,
+    EUROPE,
+    NORTH_AMERICA,
+    SOUTH_AMERICA,
+    OCEANIA
+}
